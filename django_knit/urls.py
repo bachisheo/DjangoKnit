614421d8bc4c9890b369af42e298299e18fa27b9
olga <bachisheo@gmail.com>
2022-03-13 14:56:53 +0700

    the first view was added

--- django_knit/urls.py
+++ django_knit/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+#path(route, view, kwargs=None, name=None)¶
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('knit/', include('djangoapp.urls')),
+
 ]
