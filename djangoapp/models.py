ba79b58ea8097b08d4acb3a61ed0d5a23b4dd9e0
olga <bachisheo@gmail.com>
2022-03-14 20:11:58 +0700

    fix models and create migrations

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -4,12 +4,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     id = models.IntegerField(primary_key=True, auto_created=True)
-    name = models.CharField()
-    level = models.IntegerField
+    name = models.CharField(max_length=50)
+    level = models.IntegerField()
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.RESTRICT())
+    category = models.ForeignKey(Category, on_delete=models.SET(1))
     id = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=300)
     count = models.IntegerField()
