128227e507cf06498e71674e55ccb4907309b05f
olga <bachisheo@gmail.com>
2022-03-14 20:00:02 +0700

    models were added

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField()
+    level = models.IntegerField
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT())
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=300)
+    count = models.IntegerField()
+    price = models.IntegerField(default=0)
+    description = models.TextField()
+    is_to_order = models.BooleanField()
