41ef92b8ec1b79cf12866989f32d3556fd557d8e
olga <bachisheo@gmail.com>
2022-03-14 21:28:56 +0700

    views were added

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
 from . import views
+'''
+When somebody requests a page from your website – say, “/knit/34/”, 
+Django will load the mysite.urls Python module because it’s pointed to by the ROOT_URLCONF setting.
+It finds the variable named urlpatterns and traverses the patterns in order.
+After finding the match at 'knit/', it strips off the matching text ("knit/")
+and sends the remaining text – "34/" – to the ‘knit.urls’ URLconf for further processing. 
+There it matches '<int:product_id>/', resulting in a call to the detail() view like so:
+            detail(request=<HttpRequest object>, product_id=34)
+'''
 urlpatterns = [
+    # ex: /knit/
     path('', views.index, name='index'),
+    # ex: /knit/12/
+    path('<int:product_id>', views.detail, name='detail'),
+    # ex: /knit/12/edit
+    path('<int:product_id>/edit', views.edit, name='edit'),
 ]
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hi, you're at the django_knit index.")
+
+
+def detail(request, product_id):
+    return HttpResponse("it's detail of %s." % product_id)
+
+
+def edit(request, product_id):
+    return HttpResponse("it's editing of %s." % product_id)
