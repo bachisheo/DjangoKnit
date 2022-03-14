72fa038716d5d7fa77c213fec393a45c34b8a3c7
olga <bachisheo@gmail.com>
2022-03-14 21:10:49 +0700

    fix a models

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -3,16 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     level = models.IntegerField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET(1))
-    id = models.IntegerField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=300)
     count = models.IntegerField()
     price = models.IntegerField(default=0)
     description = models.TextField()
     is_to_order = models.BooleanField()
+
+    def __str__(self):
+        return self.name
