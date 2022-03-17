20d49f317c591752af7d85fb8d6dd4d6712e3186
olga <bachisheo@gmail.com>
2022-03-17 10:34:32 +0700

    fix path to templates folter

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 from .models import Product
 
+
 # Create your views here.
 
 class IndexView(ListView):
     model = Product
     template_name = 'djangoapp/index.html'
+
 
 def detail(request, product_id):
     return HttpResponse("it's detail of %s." % product_id)
