b6abd61a1650f2345c4beec66e6a735fe3f301d0
olga <bachisheo@gmail.com>
2022-03-17 11:12:31 +0700

    added view for product details

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import ListView
 from .models import Product
@@ -12,7 +12,9 @@ class IndexView(ListView):
 
 
 def detail(request, product_id):
-    return HttpResponse("it's detail of %s." % product_id)
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'djangoapp/detail.html', {'product': product})
+
 
 
 def edit(request, product_id):
