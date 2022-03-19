7ded1131bc9618a80d58ac3b5c78ddebe8139e4b
olga <bachisheo@gmail.com>
2022-03-19 21:21:07 +0700

    fix bug with update (params for 'reverse' were not iterable)

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET(1), default='1')
+    category = models.ForeignKey('Category', on_delete=models.SET(1), default='1')
     name = models.CharField(max_length=300)
     count = models.IntegerField()
     price = models.IntegerField(default=0)
@@ -25,4 +25,4 @@ class Product(models.Model):
 
     # redirect from adding
     def get_absolute_url(self):
-        return reverse('detail', args=(str(self.id)))
+        return reverse('detail', args=[self.id])
